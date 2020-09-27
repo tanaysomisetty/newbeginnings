a6a0c7f7138cfb6d2c560ed6239ed3d18e03cf33
Tanay Somisetty <Tanay@Tanays-MacBook-Air.local>
2020-09-27 15:59:40 -0400

    first commit

---
+++ src/components/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {Redirect} from 'react-router-dom';
+import "./Home.css";
+import ReactDOM from 'react-dom';
+
+class Home extends React.Component{
+    render(){
+        return(
+            <React.Fragment>
+                <p>Hello</p>
+            </React.Fragment>
+        );
+    }
+
+}
+
+export default Home;
