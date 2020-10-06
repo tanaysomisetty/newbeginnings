25b7a56559c82a113914416ec3a0f1fde7deb637
Tanay Somisetty <Tanay@Tanays-MacBook-Air.local>
2020-10-05 23:12:21 -0400

    created navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,13 @@ import "./Header.css";
 function Header(){
     return (
         <React.Fragment>
-
+            <div className="navbar">
+                <div className="nav"><Link className="nav-link">Home</Link></div>
+                <div className="nav"><Link className="nav-link">Mentorship</Link></div>
+                <div className="nav"><Link className="nav-link">Employers</Link></div>
+                <div className="nav"><Link className="nav-link">Resources</Link></div>
+                <div className="nav"><Link className="nav-link">Sign In</Link></div>
+            </div>
         </React.Fragment>
     )
 }
